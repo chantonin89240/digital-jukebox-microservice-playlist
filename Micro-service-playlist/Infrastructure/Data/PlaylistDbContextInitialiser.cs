ba06db2fef0385ac1d69d36494e7c25144632fce
Antonin CHATEIGNER <antonin.chateigner@diiage.org>
2023-10-23 13:06:06 +0200

    ajout du using builder

--- Micro-service-playlist/Infrastructure/Data/PlaylistDbContextInitialiser.cs
+++ Micro-service-playlist/Infrastructure/Data/PlaylistDbContextInitialiser.cs
@@ -7,6 +7,8 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Data;
 
